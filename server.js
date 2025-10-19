565a60c5242707a10224d0db0f90c9b775ee7239
MousLeblanc <136707779+MousLeblanc@users.noreply.github.com>
2025-10-19 11:41:37 +0200

    Updated server.js

--- server.js
+++ server.js
@@ -154,12 +154,6 @@ mongoose.connect(mongoUri)
 app.use(errorHandler);
 
 // --- Route principale pour test Preview ---
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "index.html"));
 });
